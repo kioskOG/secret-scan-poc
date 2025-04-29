6e76c4818d254cad39f439a1dcacade23895af29
Jatin Sharma <31181135+kioskOG@users.noreply.github.com>
2025-04-29 12:32:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ print("Hey Trufflehog, some dummy secrets.")
 email_sender: "abc@gmail.com"
 email_password: "erft dfff qqqa fgyt"
 
-
 # AWS secret simulation
 AWS_ACCESS_KEY_ID = "AKIA1234567890EXAMPLE"
 AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY="
